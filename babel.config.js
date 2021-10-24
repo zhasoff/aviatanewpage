3d20561743b89b7ed6982fb8c7531529fec1c2a2
Yelzhas Yelshibekov <zhasoff@outlook.com>
2021-10-25 01:52:26 +0600

    Fix Error BabelConfig

--- babel.config.js
+++ babel.config.js
@@ -2,11 +2,4 @@ module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
   ],
-  css: {
-    loaderOptions: {
-      sass: {
-        prependData: `@import "../assets/layouts/abstracts/_variables.scss";`
-      }
-    }
-  }
 }
